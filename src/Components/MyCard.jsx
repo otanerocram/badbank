6058980ada256c9ed42866f46826d7843839f470
otaneR ocraM <renatobg82@@gmail.com>
2022-01-19 00:19:40 -0500

    cambios

--- src/Components/MyCard.jsx
+++ src/Components/MyCard.jsx
@@ -1,46 +1,54 @@
-import { Disclosure } from '@headlessui/react'
-import { ChevronUpIcon } from '@heroicons/react/solid'
-
 export default function MyCard() {
-  return (
-    <div className="w-full px-4 pt-16">
-      <div className="w-full max-w-md p-2 mx-auto bg-white rounded-2xl">
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>What is your refund policy?</span>
-                <ChevronUpIcon
-                  className={`${
-                    open ? 'transform rotate-180' : ''
-                  } w-5 h-5 text-purple-500`}
-                />
-              </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-        <Disclosure as="div" className="mt-2">
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>Do you offer technical support?</span>
-                <ChevronUpIcon
-                  className={`${
-                    open ? 'transform rotate-180' : ''
-                  } w-5 h-5 text-purple-500`}
-                />
-              </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                No.
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className="h-full w-12/12 mx-auto mb-4 my-6 md:w-12/12 shadow sm:px-10 sm:py-6 py-4 px-4 bg-white dark:bg-gray-800 rounded-md">
+                <div className="flex pb-3 items-center">
+                    <div className="-ml-1 text-gray-600 dark:text-gray-400">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="icon icon-tabler icon-tabler-trash"
+                            width={32}
+                            height={32}
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            fill="none"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <line x1={4} y1={7} x2={20} y2={7} />
+                            <line x1={10} y1={11} x2={10} y2={17} />
+                            <line x1={14} y1={11} x2={14} y2={17} />
+                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                        </svg>
+                    </div>
+                    <p className="text-lg text-gray-800 dark:text-gray-100 font-semibold pl-2">Deposit</p>
+                </div>
+                <p className="text-sm text-gray-600 dark:text-gray-400 pb-3 font-normal">
+                    Actual Balance 
+                </p>
+                <form className="w-full max-w-sm">
+                    <div className="flex items-center border-b border-teal-500 py-2">
+                        <input
+                            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                            type="text"
+                            placeholder="Jane Doe"
+                            aria-label="Full name"
+                        />
+                        <button
+                            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                            type="button"
+                        >
+                            Sign Up
+                        </button>
+                        <button className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded" type="button">
+                            Cancel
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
--- src/Pages/Deposit.jsx
+++ src/Pages/Deposit.jsx
@@ -1,11 +1,17 @@
 import { useState, useContext, useEffect } from "react";
-import MyCard from "../Components/MyCard";
-
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import Swal from "sweetalert2";
 import BankContext from "../Context/BankContext";
+import * as Yup from "yup";
 
 const Deposit = () => {
     const [signed, setSigned] = useState(false);
-    const { state, loadData } = useContext(BankContext);
+    const { state, loadData, dispatch } = useContext(BankContext);
+
+    const depositSchema = Yup.object().shape({
+        newAmmount: Yup.number()
+        .required("nm")
+    });
 
     useEffect(() => {
         loadData();
@@ -15,36 +21,71 @@ const Deposit = () => {
         state.isLogged && setSigned(true);
     }, [state.isLogged]);
 
-    return !signed ? <>Only for members</> : 
+    const today = new Date();
+    const fecha = today.toDateString();
 
-    <MyCard />
-    // state.isLoggedIn? <MyCard/>:
-    // <div className="max-w-sm w-full lg:max-w-full lg:flex">
-    //     <div
-    //         className="h-48 lg:h-auto lg:w-60 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-    //         style={{ backgroundImage: "url('https://picsum.photos/240/780/?blur')"}}
-    //         title="Woman holding a mug"
-    //     ></div>
-    //     <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-    //         <div className="mb-8">
-    //             <p className="text-sm text-gray-600 flex items-center">
-    //                 <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-    //                     <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-    //                 </svg>
-    //                 Members only
-    //             </p>
-    //             <div className="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-    //             <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-    //         </div>
-    //         <div className="flex items-center">
-    //             <img className="w-10 h-10 rounded-full mr-4" src="https://picsum.photos/seed/picsum/200/300" alt="Avatar of Jonathan Reinink" />
-    //             <div className="text-sm">
-    //                 <p className="text-gray-900 leading-none">Jonathan Reinink</p>
-    //                 <p className="text-gray-600">Aug 18</p>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
+    return (
+        <div className="max-w-sm w-full lg:max-w-full lg:flex">
+            <div
+                className="h-48 lg:h-auto lg:w-60 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                style={{ backgroundImage: "url('https://picsum.photos/240/780/?blur')" }}
+                title="Woman holding a mug"
+            ></div>
+            <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                <div className="mb-8">
+                    <p className="text-sm text-gray-600 flex items-center">
+                        <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                        </svg>
+                        Members only
+                    </p>
+                </div>
+
+                <div className="h-full w-12/12 mx-auto mb-4 my-6 md:w-12/12 shadow sm:px-10 sm:py-6 py-4 px-4 bg-white dark:bg-gray-800 rounded-md">
+                    <div className="flex pb-3 items-center">
+                        <p className="text-xl text-gray-800 dark:text-gray-100 font-semibold pl-2">Deposit</p>
+                        <br />
+                    </div>
+                    <div>
+                        <p className="text-lg text-gray-800 dark:text-gray-100 font-semibold pl-2">Actual Balance: $ {state.actualBalance}</p>
+                    </div>
+
+                    <Formik
+                        initialValues={{ newAmmount: 0 }}
+                        validationdepositSchema={depositSchema}
+                        onSubmit={(values, { setSubmitting }) => {
+                            setTimeout(() => {
+                                console.log("procesa");
+                                setSubmitting(false);
+                            }, 100);
+                        }}
+                    >
+                        <Form className="w-full max-w-sm">
+                            <div className="flex flex-col mt-8 items-left gap-2 border-b border-teal-500 py-2">
+                                <label className="mb-3 text-sm leading-none text-gray-800">newAmmount</label>
+                                <Field type="number" name="newAmmount" className="w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200" />
+                                <ErrorMessage name="newAmmount" component="div" className="text-xs text-red-500 font-bold" />
+                                <button
+                                    className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                                    type="submit"
+                                >
+                                    Deposit
+                                </button>
+                            </div>
+                        </Form>
+                    </Formik>
+                </div>
+
+                <div className="flex items-center">
+                    <img className="w-10 h-10 rounded-full mr-4" src="https://picsum.photos/seed/picsum/200/300" alt="Avatar of Jonathan Reinink" />
+                    <div className="text-sm">
+                        <p className="text-gray-900 leading-none">{state.userName}</p>
+                        <p className="text-gray-600">{fecha}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Deposit;
